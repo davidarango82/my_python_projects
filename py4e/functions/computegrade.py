7bbb9ae011acfa1d2aacb7b198db66cb53c373fa
David <davidarango82@gmail.com>
2021-12-08 09:54:26 +0100

    renamed file computegrade.py, moved to /functions

---
+++ py4e/functions/computegrade.py
@@ -0,0 +1,26 @@
+s = input("input a score between 0 and 1 to get a grade: ")
+
+def computegrade(score):
+    try:
+        score = float(score)
+        if not(0 <= score < 1):
+            return "Bad score"
+        grade = "F"
+        if score >= 0.6:
+            print("score >= 0.6")
+            grade = "D"
+        if score >= 0.7:
+            print("score >= 0.7")
+            grade = "C"
+        if score >= 0.8:
+            print("score >= 0.8")
+            grade = "B"
+        if score >= 0.9:
+            print("score >= 0.9")
+            grade = "A"
+        return grade
+    except:
+        return "Bad score"
+
+ans = computegrade(s)
+print(ans)
