56aabccfe4396dd4246ed9f0981d96e61593751e
David <davidarango82@gmail.com>
2021-12-15 20:40:31 +0100

    reimplemented computegrade function with elif

--- py4e/functions/computegrade.py
+++ py4e/functions/computegrade.py
@@ -1,4 +1,4 @@
-# Added this locally
+# This version uses elif, which makes it more efficient
 
 s = input("input a score between 0 and 1 to get a grade: ")
 
@@ -8,18 +8,23 @@ def computegrade(score):
         if not(0 <= score < 1):
             return "Bad score"
         grade = "F"
-        if score >= 0.6:
-            print("score >= 0.6")
-            grade = "D"
-        if score >= 0.7:
-            print("score >= 0.7")
-            grade = "C"
-        if score >= 0.8:
-            print("score >= 0.8")
-            grade = "B"
         if score >= 0.9:
             print("score >= 0.9")
             grade = "A"
+
+        elif score >= 0.8:
+            print("score >= 0.8")
+            grade = "B"
+
+        elif score >= 0.7:
+            print("score >= 0.7")
+            grade = "C"
+
+        elif score >= 0.6:
+            print("score >= 0.6")
+            grade = "D"
+
+
         return grade
     except:
         return "Bad score"
